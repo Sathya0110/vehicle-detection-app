9f4cc1539d9837d2621647fa4a0958d943d4b035
Sathya0110 <115555820+Sathya0110@users.noreply.github.com>
2026-03-12 15:53:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,7 @@ st.title("🚗 Smart Vehicle Detection System")
 
 @st.cache_resource
 def load_model():
-    model = YOLO("yolov8n.pt")
-    return model
+    return YOLO("yolov8n.pt")
 
 model = load_model()
 
@@ -18,13 +17,15 @@ uploaded_file = st.file_uploader("Upload Traffic Image", type=["jpg","png","jpeg
 
 if uploaded_file is not None:
 
-    image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    image = Image.open(uploaded_file).convert("RGB")
 
-    results = model(np.array(image))
+    st.image(image, caption="Uploaded Image", use_container_width=True)
+
+    results = model(np.array(image), conf=0.3)
+
     annotated = results[0].plot()
 
-    st.image(annotated, caption="Detected Vehicles")
+    st.image(annotated, caption="Detected Vehicles", use_container_width=True)
 
     vehicles = ["car","bus","truck","motorcycle"]
 
@@ -37,4 +38,4 @@ if uploaded_file is not None:
         if label in vehicles:
             count += 1
 
-    st.success(f"Total Vehicles Detected: {count}")
+    st.success(f"🚘 Total Vehicles Detected: {count}")
